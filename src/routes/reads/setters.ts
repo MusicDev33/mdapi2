dfeb8acffae4880eb4c5bdab6acc9bcae9f85f76
MusicDev33 <smccowan33@gmail.com>
2022-04-28 01:25:51 -0700

    can now change reads

---
+++ src/routes/reads/setters.ts
@@ -0,0 +1,21 @@
+import readsService from '@services/read.service';
+import { Request, Response } from 'express';
+import { Read } from '@schemas/read.schema';
+
+export const updateOneReadRoute = async (req: Request, res: Response) => {
+  const read = await readsService.findOneModelByParameter('bookId', req.params.bookId);
+
+  if (!read) {
+    return res.status(500).json({success: false, msg: `Could not find reads with bookId '${req.params.bookId}'`});
+  }
+
+  read.currentPage = req.body.currentPage;
+
+  const savedRead = await readsService.saveChangedModel(read, 'currentPage');
+
+  if (!savedRead) {
+    return res.status(500).json({success: false, msg: 'Could not save read.'});
+  }
+
+  return res.json({success: true, data: savedRead});
+};
