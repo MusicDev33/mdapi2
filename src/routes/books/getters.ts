25cfa41597d7fc34b966817cbb62d6a3be8c98ef
MusicDev33 <smccowan33@gmail.com>
2022-05-13 16:07:17 -0700

    Get book by id route

--- src/routes/books/getters.ts
+++ src/routes/books/getters.ts
@@ -13,6 +13,22 @@ export const getBooksRoute = async (req: Request, res: Response) => {
   return res.status(200).json({success: true, data: books});
 };
 
+export const getBookByIdRoute = async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  if (!id) {
+    return res.json({success: false, msg: 'Must supply valid id in URL.'});
+  }
+
+  const book = await bookService.findOneModelByQuery({_id: req.params.id});
+
+  if (!book) {
+    return res.json({success: false, msg: `Could not find book with id '${req.params.id}'`});
+  }
+
+  return res.json({success: true, data: book});
+}
+
 export const searchBooksRoute = async (req: Request, res: Response) => {
   if (!req.params.searchTerm) {
     return res.json({success: false, msg: 'Must include a search term!'});
--- src/routes/books/routes.ts
+++ src/routes/books/routes.ts
@@ -2,6 +2,8 @@ import express from 'express';
 const router = express.Router();
 import * as Routes from './export';
 
+router.get('/id/:id', Routes.getBookByIdRoute);
+
 router.post('/create', Routes.addNewBookRoute);
 router.get('/search/:searchTerm', Routes.searchBooksRoute);
 router.get('', Routes.getBooksRoute);
