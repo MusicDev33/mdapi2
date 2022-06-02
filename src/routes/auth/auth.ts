7d79b9e587566139395c676e0cf9205c39e0b12d
MusicDev33 <smccowan33@gmail.com>
2022-06-01 17:32:15 -0700

    change tolerance

--- src/routes/auth/auth.ts
+++ src/routes/auth/auth.ts
@@ -18,7 +18,7 @@ export const getAuthTokenRoute = (req: Request, res: Response) => {
 
   if (protocol === 'rhy') {
     const result = validateRhythm(password);
-    
+
     console.log(password);
     console.log(result);
 
@@ -33,7 +33,7 @@ export const getAuthTokenRoute = (req: Request, res: Response) => {
 // Rhythm Validation
 const rhythmPass = RHY_PASS.split(',') // test password
 
-const tolerance = 0.1;
+const tolerance = 0.2;
 const bpm = 120;
 const oneMin = 60000; // milliseconds in a minute
 const approvedRhythyms = [8, 12, 16];
