a504844c178f64ec187c3c218d04c0f4b6244ecd
MusicDev <smccowan33@gmail.com>
2023-03-27 22:25:19 -0700

    Can now prevent overloading of token limit

--- src/routes/zokyo/creation.ts
+++ src/routes/zokyo/creation.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from 'express';
 import { OPEN_AI_API_KEY } from '@config/constants';
 
+import { get_encoding, encoding_for_model } from '@dqbd/tiktoken';
+
 import axios from 'axios';
+
+const TOKEN_THRESHOLD = 4096;
+const encoder = encoding_for_model('gpt-3.5-turbo-0301');
 
 export const createNewChatRoute = async (req: Request, res: Response) => {
   let temperature = 1;
   let top_p = 1;
+
+  const userChats = req.body.chats;
 
   const url ='https://api.openai.com/v1/chat/completions';
   const reqConfig = {
@@ -25,7 +32,23 @@ export const createNewChatRoute = async (req: Request, res: Response) => {
   }
 
   messages.push({'role': 'system', 'content': `The date is ${getDate()} in San Francisco.`});
-  messages = messages.concat(req.body.chats);
+  messages = messages.concat(userChats);
+
+  while (!tokenCountValid(messages)) {
+    userChats.shift();
+
+    messages = [];
+
+    if (req.body.mode == 'code') {
+      temperature = 0.3;
+
+      messages.push({'role': 'user', 'content': 'You are a terse code completion machine. You will answer future questions with just code and nothing more. Do not bother explaining what the code does.'});
+      messages.push({'role': 'assistant', 'content': 'Okay, I will answer your future questions with just code snippets. Let\'s get started!'});
+    }
+
+    messages.push({'role': 'system', 'content': `The date is ${getDate()} in San Francisco.`});
+    messages = messages.concat(userChats);
+  }
 
   console.log('Chats');
 
@@ -53,6 +76,20 @@ export const createNewChatRoute = async (req: Request, res: Response) => {
     return res.status(500).json({success: false, msg: 'Something broke with Zokyo\'s backend'});
   }
 };
+
+const tokenCountValid = (messages: any[]) => {
+  let total = 0;
+
+  for (let message of messages) {
+    total += encoder.encode(message.content).length;
+  }
+
+  if (total > TOKEN_THRESHOLD) {
+    return false;
+  }
+
+  return true;
+}
 
 // Written by ChatGPT!
 const getDate = (): string => {
