70e96ae5a6d4563b0b021e67e6c221f89ec70834
MusicDev <smccowan33@gmail.com>
2023-07-18 23:43:39 -0700

    Separate data by name of URL

--- src/routes/xenon/creation.ts
+++ src/routes/xenon/creation.ts
@@ -4,6 +4,11 @@ import { Request, Response } from "express";
 
 import { parseSite } from './parse';
 
+interface SiteData {
+  name: string;
+  content: string;
+}
+
 export const getParsedUrlRoute = async (req: Request, res: Response) => {
   if (!req.query.url) {
     return res.status(404).json({success: false, msg: 'Must supply a valid URL!'});
@@ -15,6 +20,8 @@ export const getParsedUrlRoute = async (req: Request, res: Response) => {
   let url: string = req.query.url as string;
   console.log(url.split('/'));
 
+  let data: SiteData[] = [];
+
   let hasNextAnchor = true;
   while (hasNextAnchor) {
     visitedUrls.push(url);
@@ -23,10 +30,12 @@ export const getParsedUrlRoute = async (req: Request, res: Response) => {
     const root = parse(html);
     
     const pagePTags = root.getElementsByTagName('p');
+    let currentPTags: string[] = [];
     for (let pTag of pagePTags) {
       for (let pTagChild of pTag.childNodes) {
         if (pTagChild.nodeType == 3 && pTagChild.rawText.trim() != '') {
-          pTags.push(`<p>${pTagChild.rawText.trim()}</p>`)
+          pTags.push(`<p>${pTagChild.rawText.trim()}</p>`);
+          currentPTags.push(`<p>${pTagChild.rawText.trim()}</p>`);
         }
       }
     }
@@ -38,10 +47,21 @@ export const getParsedUrlRoute = async (req: Request, res: Response) => {
     }
 
     const splitUrl = url.split('/');
+    const currentUrlName = splitUrl[splitUrl.length - 1];
     splitUrl[splitUrl.length - 1] = nextAnchor['href'];
     url = splitUrl.join('/');
+
+    data.push({
+      name: currentUrlName,
+      content: currentPTags.join('\n')
+    })
   }
   
-  res.set('Content-Type', 'text/html');
-  return res.send(pTags.join('\n'));
+  // res.set('Content-Type', 'text/html');
+  // return res.send(pTags.join('\n'));
+
+  return res.json({
+    success: true,
+    data: data
+  });
 }
