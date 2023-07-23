97132ba13828b5b22754443fbf3df4fe91928355
MusicDev <smccowan33@gmail.com>
2023-07-22 19:15:59 -0700

    Removed gentools

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -4,3 +4,21 @@ MDAPI has needed a CLI fora long time. It's about time I finally add one and sav
 
 // tslint:disable-next-line
 require('tsconfig-paths/register');
+
+import figlet from 'figlet';
+import { Command } from 'commander';
+
+const program = new Command();
+
+console.log(figlet.textSync('MDAPI2 CLI'));
+program
+  .version('0.1.0')
+  .description('A CLI for managing various aspects of MDAPI2')
+  .option('-g, --gen <value>', 'Generate interface, schema, and service for a new object')
+  .parse(process.argv);
+
+const options = program.opts();
+
+if (options.gen) {
+  console.log(options.gen);
+}
