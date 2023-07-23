da160d36f7e1860e6d5224627474be0304430263
MusicDev <smccowan33@gmail.com>
2023-07-22 19:00:37 -0700

    Update packages

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -1,2 +1,6 @@
+/*
+MDAPI has needed a CLI fora long time. It's about time I finally add one and save myself a lot of time in the future.
+*/
+
 // tslint:disable-next-line
 require('tsconfig-paths/register');
