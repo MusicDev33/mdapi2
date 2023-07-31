a7070bc1ac5ed4cdda3b6f4296bf3e374b2c2e03
MusicDev <smccowan33@gmail.com>
2023-07-30 22:15:01 -0700

    Get xsources

--- src/routes/xenon/getters.ts
+++ src/routes/xenon/getters.ts
@@ -1,14 +1,23 @@
-import axios from 'axios';
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 import { parseSite } from './parse';
 
 import { IXRawSection } from '@models/xrawsection.model';
 import { XRawSection } from '@schemas/xrawsection.schema';
 
 import xRawSectionService from '@services/xrawsection.service';
+import xSourceService from '@services/xsource.service';
 
-const getAllXRawSections = (req: Request, res: Response) => {
+export const getAllXRawSectionsRoute = async (req: Request, res: Response) => {
   const sections = ''
 
   return res.json({success: true});
+}
+
+export const getAllXSourcesRoute = async (req: Request, res: Response) => {
+  const sources = await xSourceService.findModelsByQuery({}, {}, 100);
+  if (!sources) {
+    return res.status(500).json({success: false, msg: 'Something went wrong, but it\'s not on your end!'});
+  }
+
+  return res.json({success: true, msg: 'Success', data: sources});
 }
--- src/routes/xenon/routes.ts
+++ src/routes/xenon/routes.ts
@@ -3,5 +3,6 @@ const router = express.Router();
 import * as Routes from './export';
 
 router.get('/parse', Routes.createXRawSectionRoute);
+router.get('/xsources/all', Routes.getAllXSourcesRoute);
 
 export default router;
