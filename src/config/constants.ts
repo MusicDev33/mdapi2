ba95d96a8100074565a27a3e044a791a365209bd
MusicDev33 <smccowan33@gmail.com>
2025-10-27 23:42:14 -0600

    Fixed .env check

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,28 +1,29 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const dotenvCheck = (property: string | undefined): string => {
-  if (!property) {
-    throw new Error(`dotenv check failed on property`);
+const updatedDotenvCheck = (property: string): string => {
+  const prop = process.env[property];
+  if (!prop) {
+    throw new Error(`dotenv check failed on property ${property}`);
   }
-
-  return property;
+  
+  return prop;
 }
 
-export const PORT = dotenvCheck(process.env.PORT);
-export const AUTH_TOKEN = dotenvCheck(process.env.AUTH_TOKEN);
-export const API_BASE = dotenvCheck(process.env.API_BASE);
-export const DB_NAME = dotenvCheck(process.env.DB_NAME);
-export const USER_PASS = dotenvCheck(process.env.USER_PASS);
-export const RHY_PASS = dotenvCheck(process.env.RHY_PASS);
-export const WHITELIST_CORS = dotenvCheck(process.env.WHITELIST_CORS).split(',');
-export const OPEN_AI_API_KEY = dotenvCheck(process.env.OPEN_AI_API_KEY);
-export const ANTHROPIC_API_KEY = dotenvCheck(process.env.ANTHROPIC_API_KEY);
-export const DEEPSEEK_API_KEY = dotenvCheck(process.env.DEEPSEEK_API_KEY);
-export let WHITELIST_USERS = dotenvCheck(process.env.WHITELIST_USERS).split(',');
+export const PORT = updatedDotenvCheck('PORT');
+export const AUTH_TOKEN = updatedDotenvCheck('AUTH_TOKEN');
+export const API_BASE = updatedDotenvCheck('API_BASE');
+export const DB_NAME = updatedDotenvCheck('DB_NAME');
+export const USER_PASS = updatedDotenvCheck('USER_PASS');
+export const RHY_PASS = updatedDotenvCheck('RHY_PASS');
+export const WHITELIST_CORS = updatedDotenvCheck('WHITELIST_CORS').split(',');
+export const OPEN_AI_API_KEY = updatedDotenvCheck('OPEN_AI_API_KEY');
+export const ANTHROPIC_API_KEY = updatedDotenvCheck('ANTHROPIC_API_KEY');
+export const DEEPSEEK_API_KEY = updatedDotenvCheck('DEEPSEEK_API_KEY');
+export let WHITELIST_USERS = updatedDotenvCheck('WHITELIST_USERS').split(',');
 
-export const M_SITE_URL = dotenvCheck(process.env.M_SITE_URL);
-export const R_SITE_URL = dotenvCheck(process.env.R_SITE_URL)
+export const M_SITE_URL = updatedDotenvCheck('M_SITE_URL');
+export const R_SITE_URL = updatedDotenvCheck('R_SITE_URL')
 
 export const ACCEPTED_AGENTS = ['MDAPIWebv1', 'MDReads'];
 
